c69ea981ced3b793e7dcc4a35ee12ae90c665279
97067883Ludo <97067883@st.deltion.nl>
2021-11-29 15:10:59 +0100

    readxlsx.php simplexlsx

--- readxlsx.php
+++ readxlsx.php
@@ -1,6 +1,35 @@
 <?php
+require 'vendor/shuchkin/simplexlsx/src/SimpleXLSX.php';
 
+$file = 'wedstrijden.xlsx';
 
+$xlsx = new SimpleXLSX($file);
+
+/*if ( $xlsx = SimpleXLSX::parse($file)) {
+    echo"yooo";
+}
+else{
+    echo "fout";
+}*/
+foreach ($xlsx->rows() as $row => $value) {
+
+    /*if ($row == 0) {
+        $header = $value;
+        $headerSize = count($header);
+        continue;
+    }
+    echo"Wedstrijd: <br />";
+    for ($i=0; $i < $headerSize; $i++) {
+        echo $header[$i];
+        echo ': ';
+        echo $value[$i];
+        echo '<br />';
+    }*/
+    echo "row $row ";
+    print_r($value);
+    echo"<br />";
+
+}
 
 
 ?>
