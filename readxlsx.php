dc50ea432e79429e22c5d04b85080f5a8462d5c2
97067883Ludo <97067883@st.deltion.nl>
2021-11-29 16:25:08 +0100

    fix date time fucntion added

--- readxlsx.php
+++ readxlsx.php
@@ -13,7 +13,7 @@ else{
 }*/
 foreach ($xlsx->rows() as $row => $value) {
 
-    /*if ($row == 0) {
+    if ($row == 0) {
         $header = $value;
         $headerSize = count($header);
         continue;
@@ -21,14 +21,41 @@ foreach ($xlsx->rows() as $row => $value) {
     echo"Wedstrijd: <br />";
     for ($i=0; $i < $headerSize; $i++) {
         echo $header[$i];
+        //kijk of de het een datum of tijd is, dit moet gefixed worden
+        if ($header[$i] == "Datum" || $header[$i] == "Tijd") {
+            
+            //call fixDateTime function om datum en tijd te fixen
+           $fixedTime = fixDateTime($value[$i], $header[$i]);
+
+           //als de date time fix is gefaald dan trow error en die
+           if (!$fixedTime) {
+               echo "error fix time";
+               die;
+           }
+            echo ': ';
+            echo $fixedTime;
+            echo '<br />';
+        }else{
         echo ': ';
         echo $value[$i];
         echo '<br />';
-    }*/
-    echo "row $row ";
-    print_r($value);
-    echo"<br />";
+        }
+    }
+    //echo "row $row ";
+    //print_r($value);
+    //echo"<br />";
 
+}
+function fixDateTime($dateTime, $hoursOrDate){
+    //echo $hoursOrDate;
+    $explodedDate = explode(" ", $dateTime);
+    //print_r($explodedDate);
+    if ($hoursOrDate == "Datum") {
+     return  $explodedDate[0];
+    }else{
+        return $explodedDate[1];
+    }
+    return FALSE;
 }
 
 
