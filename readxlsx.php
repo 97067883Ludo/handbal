2d97d6c98fe9ba4a7b18674d15d2aae1d86afe72
97067883Ludo <97067883@st.deltion.nl>
2021-12-01 13:54:19 +0100

    added header

--- readxlsx.php
+++ readxlsx.php
@@ -10,6 +10,7 @@ $xlsx = new SimpleXLSX($file);
 global $errors;
 $errors = array();
 
+
 foreach ($xlsx->rows() as $row => $value) {
     //get header
     if ($row == 0) {
@@ -17,30 +18,28 @@ foreach ($xlsx->rows() as $row => $value) {
         $headerSize = count($header);
         continue;
     }
-    
-    echo"Wedstrijd: <br />";
-    
+    //echo"Wedstrijd: <br />";
     for ($i=0; $i < $headerSize; $i++) {
-        echo $header[$i];
+        //array_push($wedstrijd, $regel);
+        //echo $header[$i];
         //kijk of de het een datum of tijd is, dit moet gefixed worden
         if ($header[$i] == "Datum" || $header[$i] == "Tijd") {
-
             //call fixDateTime function om datum en tijd te fixen
             $fixedTime = fixDateTime($value[$i], $header[$i], $row, $errors);
-            echo': ';
+            //$regel = array($header[$i] => $fixedTime);
+            /*echo': ';
             echo $fixedTime;
-            echo '<br />';
+            echo '<br />';*/
         }else{
-            echo ': ';
+            /*echo ': ';
             echo $value[$i];
-            echo '<br />';
+            echo '<br />';*/
         }
     }
 }
 
 
-
-function fixDateTime($dateTime, $hoursOrDate, $row, $errors){
+function fixDateTime($dateTime, $hoursOrDate, $row){
 
     try {
         //probeer de $dateTime om te zetten naar een dateTime data type
@@ -69,5 +68,64 @@ function fixDateTime($dateTime, $hoursOrDate, $row, $errors){
     }
 }
 
-var_dump($errors);
+function writeHeader($header, $headerSize, $xlsx){
+    $errors = $GLOBALS['errors'];
+    foreach ($errors as $key => $value) {
+        echo '
+        <div class="alert alert-warning" role="alert">
+            fout gedetecteerd op '.$value.'
+        </div>
+        ';
+    }
+    echo'
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        ';
+    for ($i=0; $i < $headerSize; $i++) { 
+        if ($header[$i] == 'Nummer' || $header[$i] == 'Accommodatie' || $header[$i] == 'Plaats' || $header[$i] == 'Opm PR') {
+            
+        }else{
+            echo '
+                    <th scope="col">'.$header[$i].'</th>
+            ';
+        }
+    }
+    echo'
+        </tr>
+    </thead>
+    <tbody>
+    ';
+    walkThruxlsx($xlsx);
+}
+
+function walkThruxlsx($xlsx){
+}
+
+
+
+//var_dump($errors);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>    
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+        <h1>Wedstrijd maker Handbal Haarle</h1>
+        <h2>Hoofdpagina</h2>
+    </div>
+    <div class="container mt-5">
+    <?php
+    writeHeader($header, $headerSize, $xlsx);
+    ?>
+    </div>
+</body>
+</html>
