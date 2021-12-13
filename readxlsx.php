d4fd4874c6ab6ef912c6e7a9ef3893ff6703998b
Ludo Tielbeke <97067883@st.deltion.nl>
2021-12-13 14:20:50 +0100

    added indexxlsx voor overzichtelijkheid

--- readxlsx.php
+++ readxlsx.php
@@ -6,13 +6,11 @@ $file = 'wedstrijden.xlsx';
 $xlsx = new SimpleXLSX($file);
 
 //declareer error array voor als er fouten voorkomen bij het inladen van het xlsx bestand
-
 global $errors;
-
 $errors = array();
 
+//wedstrijd array deze array wordt gevuld met alle wedstrijden
 $wedstrijd = array();
-
 global $wedstrijd;
 
 foreach ($xlsx->rows() as $row => $value) {
@@ -21,6 +19,7 @@ foreach ($xlsx->rows() as $row => $value) {
         global $headerSize;
         global $header;
         $header = $value;
+        $header = array_map('strtolower', $header);
         $headerSize = count($header);
         continue;
     }
@@ -29,15 +28,14 @@ foreach ($xlsx->rows() as $row => $value) {
         //array_push($wedstrijd, $regel);
         //echo $header[$i];
         //kijk of de het een datum of tijd is, dit moet gefixed worden
-        if ($header[$i] == "Datum" || $header[$i] == "Tijd") {
+        if ($header[$i] == "datum" || $header[$i] == "tijd") {
             //call fixDateTime function om datum en tijd te fixen
             $fixedTime = fixDateTime($value[$i], $header[$i], $row, $errors);
 
             $kolom[$header[$i]] = $fixedTime;
         }else{
-
             $kolom[$header[$i]] = $value[$i];
-            
+
         }
     }
     $rij[] = $kolom;
@@ -62,10 +60,10 @@ function fixDateTime($dateTime, $hoursOrDate, $row){
     }
     
     switch ($hoursOrDate) {
-        case 'Datum':
+        case 'datum':
             return $fixedTime->format("Y-m-d");
             break;
-        case 'Tijd':
+        case 'tijd':
             return $fixedTime->format("H:i");
             break;
         default:
@@ -79,7 +77,7 @@ function writeHeader($header, $headerSize){
     $errors = $GLOBALS['errors'];
     foreach ($errors as $key => $value) {
         echo '
-        <div class="alert alert-warning" role="alert">
+        <div class="alert alert-danger" role="alert">
             fout gedetecteerd op '.$value.'
         </div>
         ';
@@ -91,11 +89,11 @@ function writeHeader($header, $headerSize){
         <th scope="col">#</th>
         ';
     for ($i=0; $i < $headerSize; $i++) { 
-        if ($header[$i] == 'Nummer' || $header[$i] == 'Accommodatie' || $header[$i] == 'Plaats' || $header[$i] == 'Opm PR' || $header[$i] ==  NULL) {
+        if ($header[$i] == 'nummer' || $header[$i] == 'accommodatie' || $header[$i] == 'plaats' || $header[$i] == 'opm pr' || $header[$i] ==  NULL) {
             
         }else{
             echo '
-                    <th scope="col">'.$header[$i].'</th>
+            <th scope="col">'.$header[$i].'</th>
             ';
         }
     }
@@ -117,9 +115,9 @@ function walkThruxlsx(){
         echo'<th> <input type="checkbox" name="product[]" value="' . $i . '" id=""> </th>';
         for ($i=0; $i < $headerSize; $i++) { 
             
-            if ($header[$i] == 'Datum' || $header[$i] == 'Tijd' || $header[$i] == 'Thuisteam' 
-                || $header[$i] == 'Uitteam' || $header[$i] == 'Scheidsrechter-1' || $header[$i] == 'Scheidsrechter-2' 
-                || $header[$i] == 'Zaaldienst') {
+            if ($header[$i] == 'datum' || $header[$i] == 'tijd' || $header[$i] == 'thuisteam' 
+                || $header[$i] == 'uitteam' || $header[$i] == 'scheidsrechter-1' || $header[$i] == 'scheidsrechter-2' 
+                || $header[$i] == 'zaaldienst') {
                 echo '<td>'.$value[$key][$header[$i]].'</td>';
                 
             }
@@ -133,29 +131,3 @@ function walkThruxlsx(){
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Document</title>
-</head>
-<body>    
-    <div class="container-fluid p-5 bg-primary text-white text-center">
-        <h1>Wedstrijd maker Handbal Haarle</h1>
-        <h2>Hoofdpagina</h2>
-    </div>
-    <div class="container mt-5">
-    <form action="afvangenxlsx.php" method="post">
-    <input type="submit" class="btn btn-success button" value="Verder ->">
-    <?php
-    writeHeader($header, $headerSize);
-    walkThruxlsx();
-    ?>
-    </form>
-    </div>
-</body>
-</html>
