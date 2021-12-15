a28f5121733835b76dc3705ad50603aee2385dcd
Ludo Tielbeke <97067883@st.deltion.nl>
2021-12-15 16:42:02 +0100

    added functioning navbar

--- printMenu.php
+++ printMenu.php
@@ -4,10 +4,10 @@ echo '
 <nav class="navbar navbar-expand-sm bg-light justify-content-center">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#">Lijst</a>
+      <a class="nav-link" href="index.php">Lijst</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Archief</a>
+      <a class="nav-link" href="archive.php">Archief</a>
     </li>
   </ul>
 </nav>
