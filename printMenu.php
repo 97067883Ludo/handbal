71376fc192e2f32016324819904020d373b5afef
Ludo Tielbeke <97067883@st.deltion.nl>
2021-12-20 09:07:47 +0100

    added file upload.php printMenu.php

--- printMenu.php
+++ printMenu.php
@@ -10,28 +10,29 @@ echo '
       <a class="btn btn-primary" href="archive.php">Archief</a>
     </li>
     <li class="nav-item">
-    <a class="btn btn-primary" href="#" data-bs-toggle="modal" data-bs-target="#myModal">NieuweLijst</a>
-      <!-- The Modal -->
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
+      <a class="btn btn-primary" href="#" data-bs-toggle="modal" data-bs-target="#myModal">NieuweLijst</a>
+          <!-- The Modal -->
+      <div class="modal" id="myModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
 
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Nieuwe lijst uploaden</h4>
-          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <h4 class="modal-title">Nieuwe lijst uploaden</h4>
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+              <form action="upload.php" method="post" enctype="multipart/form-data">
+                
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+              </form>
+            </div>
+          </div>
         </div>
-
-        <!-- Modal body -->
-        <div class="modal-body">
-          Modal body..
-        </div>
-
-        <!-- Modal footer -->
-
       </div>
-    </div>
-  </div>
     </li>
   </ul>
 </nav>
