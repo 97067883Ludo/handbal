616cc29cc3ce8d9d0a27a6d162bd4837e4813601
Ludo Tielbeke <97067883@st.deltion.nl>
2021-12-21 09:19:01 +0100

    upload module finished

--- upload.php
+++ upload.php
@@ -1,14 +1,23 @@
 <?php
-if ($_FILES['fileToUpload']['name'] == null) {
-    header('Location: index.php?Upload=1');
-    $uploadOk = 0;
-    return;
-}
-
 $target_dir = 'upload/';
 $target_file = $target_dir . basename($_FILES['fileToUpload']['name']);
 $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$uploadOk =1;
+$uploadOk = 1;
+print_r($_FILES);
+if ($_FILES['fileToUpload']['name'] == null) {
+    $uploadOk = 0;
+    header('Location: index.php?Upload=1');
+    return;
+}
+
+$maxSize = $_FILES['fileToUpload']['size'];
+//echo $maxSize;
+
+if ($maxSize > 200000) {
+    $uploadOk = 0;
+    header('Location: index.php?Upload=3');
+    return;
+}
 
 if ($fileType != 'xlsx') {
     $uploadOk = 0;
@@ -16,8 +25,11 @@ if ($fileType != 'xlsx') {
     return;
 }
 
-move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 
-header("Location: index.php?Upload=0");
+if ($uploadOk == 1) {
+    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+    rename($target_file, $target_dir . 'wedstrijden.xlsx');
+    header("Location: index.php?Upload=0"); 
+}
 
 ?>
