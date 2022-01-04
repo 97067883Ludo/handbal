c9bccc0d9e09ff7afb20563c084458911aa04762
Ludo Tielbeke <97067883@st.deltion.nl>
2022-01-04 16:56:36 +0100

    mail settings json

--- mail.php
+++ mail.php
@@ -5,6 +5,11 @@ $subject = $_GET['subject'];
 $message = $_GET['message'];
 $file = $_GET['file'];
 
+
+$mailSettings = file_get_contents('config.json');
+
+$mailSettings = json_decode($mailSettings, true);
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -15,36 +20,36 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'ludotielbeke@outlook.com';                     //SMTP username
-    $mail->Password   = 'Ludo123456';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //get from settings.json
+    $mail->isSMTP();
+    $mail->Host       = $mailSettings['mailSettings'][0]['host'];
+    $mail->SMTPAuth   = $mailSettings['mailSettings'][0]['SMTPAuth'];
+    $mail->Username   = $mailSettings['mailSettings'][0]['Username'];
+    $mail->Password   = $mailSettings['mailSettings'][0]['Password'];
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = $mailSettings['mailSettings'][0]['Port'];
 
     //Recipients
     $mail->setFrom($mail->Username);
-    $mail->addAddress($recipient);     //Add a recipient
+    $mail->addAddress($recipient);
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);
     $mail->Subject = $subject;
     $mail->Body    = $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->addAttachment($file, 'Schemalijst.pdf');         //Add attachments
-
+    $mail->addAttachment($file, 'Schemalijst.pdf');
     $mail->send();
     echo '<div class="alert alert-success" role="alert">
-        De mail is succesvol verstuurd
-        </div>
+            De mail is succesvol verstuurd
+          </div>
     ';
 } catch (Exception $e) {
 
     echo '<div class="alert alert-danger" role="alert">
-        Message could not be sent. Mailer Error: {'.$mail->ErrorInfo.'}
-        </div>
-        ';
+            Message could not be sent. Mailer Error: {'.$mail->ErrorInfo.'}
+          </div>
+    ';
 }
 ?>
