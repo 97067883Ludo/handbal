b62682bd015941bb0175fbc286674654611c0925
ludotielbeke <ludotielbeke@gmail.com>
2022-11-23 15:45:42 +0100

    it dit work jsut doing the thing

--- resources/js/components/tafeldienst/search.vue
+++ resources/js/components/tafeldienst/search.vue
@@ -9,7 +9,7 @@
                         :value="modelValue"
                         @input="$emit('update:modelValue', $event.target.value)"
                         class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                        type="text" placeholder="Uw voor en achternaam in" aria-label="Uw Naam">
+                        type="text" placeholder="Voer uw voor en achternaam in" aria-label="Uw Naam">
                     <button
                         @click="$emit('search')"
                         class=" h-full flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-r-lg"
